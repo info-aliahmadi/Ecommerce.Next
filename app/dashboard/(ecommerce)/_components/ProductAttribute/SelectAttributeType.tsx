6502d55b04a8db823a168ef1db2ae880dc1d8b17
info.aliahmadi <info.aliahmadi@gmail.com>
2026-09-02 20:08:56 +0330

    change enum selector

--- app/dashboard/(ecommerce)/_components/ProductAttribute/SelectAttributeType.tsx
+++ app/dashboard/(ecommerce)/_components/ProductAttribute/SelectAttributeType.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { FormControl, InputLabel } from '@mui/material';
-import { useTranslations } from 'next-intl';
 import EnumDropdown from '@dashboard/_components/EnumDropdown';
-import AttributeTypes from '@root/app/types/enums/AttributeType';
+import AttributeTypes, { attributeTypeLabelKeys } from '@root/app/types/enums/AttributeType';
 
 interface SelectAttributeTypeProps {
   defaultValue?: AttributeTypes | null;
@@ -23,32 +22,18 @@ const SelectAttributeType: React.FC<SelectAttributeTypeProps> = ({
   disabled = false,
   showNoneOption = false,
 }) => {
-  const t = useTranslations('');
 
   const handleChange = (newValue: AttributeTypes | null) => {
     setFieldValue(name, newValue);
   };
 
-  const attributeLabels: Record<string, string> = {
-    [AttributeTypes.Color]: t('fields.productAttribute.attributeTypes.Color'),
-    [AttributeTypes.Size]: t('fields.productAttribute.attributeTypes.Size'),
-    [AttributeTypes.Weight]: t('fields.productAttribute.attributeTypes.Weight'),
-    [AttributeTypes.Length]: t('fields.productAttribute.attributeTypes.Length'),
-    [AttributeTypes.Width]: t('fields.productAttribute.attributeTypes.Width'),
-    [AttributeTypes.Height]: t('fields.productAttribute.attributeTypes.Height'),
-    [AttributeTypes.Material]: t('fields.productAttribute.attributeTypes.Material'),
-    [AttributeTypes.Style]: t('fields.productAttribute.attributeTypes.Style'),
-    [AttributeTypes.Pattern]: t('fields.productAttribute.attributeTypes.Pattern'),
-    [AttributeTypes.Brand]: t('fields.productAttribute.attributeTypes.Brand'),
-    [AttributeTypes.Model]: t('fields.productAttribute.attributeTypes.Model'),
-  };
   return (
     <FormControl error={error} key={name} fullWidth>
       <InputLabel id={`${name}-label`}>{label}</InputLabel>
       <EnumDropdown
+        disabled={disabled}
         defaultValue={defaultValue ?? undefined}
-        enumObject={AttributeTypes}
-        customLabels={attributeLabels}
+        customLabelKeys={attributeTypeLabelKeys}
         onChange={handleChange}
         showNoneOption={showNoneOption}
         noneOptionLabel="-"
